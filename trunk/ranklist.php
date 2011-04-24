82a3cf2b9c11a1aaa777eaee9c4d13035bc0ce67
xiaoshaw@gmail.com <xiaoshaw@gmail.com@c8cccfd0-c322-43fa-c535-e899d0090043>
2011-04-24 05:10:51 +0000

    correct monday

--- trunk/ranklist.php
+++ trunk/ranklist.php
@@ -41,8 +41,8 @@
                                       $s=date('Y').'-'.date('m').'-'.date('d');
                                         break;
 				case 'w': 
-                                       $monday=mktime(0, 0, 0, date("m"),date("d")-date("w"), date("Y"));
-                                        $s=strftime("%Y-%m-%d",$monday);
+                                        $monday=mktime(0, 0, 0, date("m"),date("d")-(date("w")+7)%8+1, date("Y"));
+                                          $s=strftime("%Y-%m-%d",$monday);
                                            break;
 				case 'm': 
 				    $s=date('Y').'-'.date('m').'-01';break;
