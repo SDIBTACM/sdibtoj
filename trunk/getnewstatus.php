5fb8cad7ff5255c22902f8d307d784fb84064d9b
wilsonjuxta@gmail.com <wilsonjuxta@gmail.com@c8cccfd0-c322-43fa-c535-e899d0090043>
2014-07-01 12:59:54 +0000

    管理员管理报名信息

---
+++ trunk/getnewstatus.php
@@ -0,0 +1,39 @@
+<?
+	require_once("./include/db_info.inc.php");
+	$to=$_GET['to'];
+	$cid=intval($_GET['cid']);
+	$user_id=mysql_real_escape_string($_GET['user']);
+	$pending=intval($to);
+	if($pending==1)
+		$response="<font color=red>Accepted";
+	else if($pending==-1)
+		$response="<font color=gray>Rejected";
+$sql="UPDATE `contestreg` SET `ispending`='$pending' WHERE `contestreg`.`user_id`='".$user_id."' AND `contestreg`.`contest_id`='$cid'";
+	mysql_query($sql) or die(mysql_error());
+$sql_1="SELECT count(*) FROM `privilege` WHERE `privilege`.`user_id`='".$user_id."' AND `privilege`.`rightstr`='c$cid'";
+	$result=mysql_query($sql_1);
+	$row=mysql_fetch_array($result);
+	$pid_cnt=intval($row[0]);
+	mysql_free_result($result);
+	if($pid_cnt)
+	{
+		if($pending==1)
+			;
+		else if($pending==-1)
+		{
+			$sql="DELETE FROM `privilege` WHERE `privilege`.`user_id`='".$user_id."' AND `privilege`.`rightstr`='c$cid'";
+			mysql_query($sql) or die(mysql_error());
+		}
+	}
+	else
+	{
+		if($pending==1)
+		{
+			$sql="INSERT INTO `privilege`(`user_id`,`rightstr`) VALUES ('".$user_id."','c$cid')";
+			mysql_query($sql) or die(mysql_error());
+		}
+		else if($pending==-1)
+			;
+	}
+	echo	$response;
+?>
