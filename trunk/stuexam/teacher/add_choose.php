1281a2980ff28efc539f326471723d1146448c10
wilsonjuxta@gmail.com <wilsonjuxta@gmail.com@c8cccfd0-c322-43fa-c535-e899d0090043>
2014-10-22 06:00:21 +0000

    去除对转义字符的处理

--- trunk/stuexam/teacher/add_choose.php
+++ trunk/stuexam/teacher/add_choose.php
@@ -4,7 +4,7 @@
 <?
 	function test_input($data){
 		$data = trim($data);
-  		$data = stripslashes($data);
+  		//$data = stripslashes($data);
   		$data = htmlspecialchars($data);
   		$data = mysql_real_escape_string($data);
   		return $data;
