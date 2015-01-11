83e7a7fd6b49f377de96b6e211722a6d36c882f9
wilsonjuxta@gmail.com <wilsonjuxta@gmail.com@c8cccfd0-c322-43fa-c535-e899d0090043>
2015-01-11 14:04:00 +0000

    add sortPage

--- stuexam/teacher/db.func.php
+++ stuexam/teacher/db.func.php
@@ -35,4 +35,59 @@ function fetchOne($sql){
 	mysql_free_result($result);
 	return $row;
 }
+
+//SortStuScore
+function SortStuScore($table){
+	$sqladd = "";
+	$where = array();
+	$whereflag = false;
+	$order = array();
+	$orderflag = false;
+	if(isset($_GET['xsid']))
+	{
+		$xsid = $_GET['xsid'];
+		$xsid = mysql_real_escape_string($xsid);
+		$where[] = "{$table}.user_id like '%{$xsid}%'";
+	}
+	if(isset($_GET['xsname']))
+	{
+		$xsname = $_GET['xsname'];
+		$xsname = mysql_real_escape_string($xsname);
+		$where[] = "{$table}.nick like '%{$xsname}%'";
+	}
+	if(isset($_GET['sortanum']))
+	{
+		$sortanum = intval($_GET['sortanum']);
+		if($sortanum&1) $order[]="choosesum ASC";
+		if($sortanum&2) $order[]="judgesum ASC";
+		if($sortanum&4) $order[]="fillsum ASC";
+		if($sortanum&8) $order[]="programsum ASC";
+		if($sortanum&16) $order[]="score ASC";
+	}
+	if(isset($_GET['sortdnum']))
+	{
+		$sortdnum = intval($_GET['sortdnum']);
+		if($sortdnum&1) $order[]="choosesum DESC";
+		if($sortdnum&2) $order[]="judgesum DESC";
+		if($sortdnum&4) $order[]="fillsum DESC";
+		if($sortdnum&8) $order[]="programsum DESC";
+		if($sortdnum&16) $order[]="score DESC";
+	}
+	if(!empty($where[0]))
+	{
+		$where = join(' AND ',$where);
+		$where = " WHERE ".$where;
+	}
+	else
+		$where = join('',$where);
+	if(!empty($order[0]))
+	{
+		$order = join(',',$order);
+		$order = "ORDER BY ".$order;
+	}
+	else
+		$order = join('',$order);
+	$sqladd = $where." ".$order;
+	return $sqladd;
+}
 ?>
