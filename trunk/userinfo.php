c4f757d84c0b5ad1db849e79a8e067d060a6fd84
xiaoshaw@gmail.com <xiaoshaw@gmail.com@c8cccfd0-c322-43fa-c535-e899d0090043>
2015-02-05 15:47:47 +0000

    更改IP库以及显示属地

--- trunk/userinfo.php
+++ trunk/userinfo.php
@@ -152,8 +152,10 @@ mysql_free_result($result);
 
 </table>
 <?
+require_once('./include/IP.class.php');
+$ip  =new IP();
 if (isset($_SESSION['administrator'])){
-$sql="SELECT * FROM `loginlog` WHERE `user_id`='$user_mysql' order by `time` desc LIMIT 0,10";
+$sql="SELECT * FROM `loginlog` WHERE `user_id`='$user_mysql' order by `time` desc LIMIT 0,30";
 $result=mysql_query($sql) or die(mysql_error());
 echo "<table border=1>";
 echo "<tr align=center><td>UserID<td>Password<td>IP<td>Time<td>Delete</tr>";
@@ -162,6 +164,21 @@ for (;$row=mysql_fetch_row($result);){
 	echo "<td>".$row[0];
 	echo "<td>".$row[1];
 	echo "<td>".$row[2];
+        $l =$ip->find($row[2]);
+      if(!strcmp(trim($l[0]),"局域网"))
+         echo "<td>局域网";
+      else
+      {
+      if(strlen(trim($l[1]))==0)
+          echo "<td>".$l['0'];
+      else
+       {
+           if(!strcmp($l[1],$l[2]))
+              echo "<td>".$l[1].'@'.$l[0];
+           else
+              echo "<td>".$l[1].$l[2].$l[3].'@'.$l[0];
+       }
+        }
 	echo "<td>".$row[3];
         echo "<td><a href=./admin/delete_log.php?uid=".$row[0]."&logtime=".strtotime($row[3]).">delete</a>";
 	echo "</tr>";
