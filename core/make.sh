e587d93540091ad3f26820c28819fb76822408db
boxjan <boxjan@outlook.com>
2018-09-18 11:43:54 +0800

    update judge core

--- core/make.sh
+++ core/make.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 cd judged
 make
+chmod +x judged
 cp judged /usr/bin
 cd ../judge_client
 make
+chmod +x judge_client
 cp judge_client /usr/bin
-cd ../sim/sim_2_70
-make sim_c
-make sim_java
-make sim_pasc
-cp sim_c /usr/bin
-cp sim_java /usr/bin/sim_java
-cp sim_pasc /usr/bin/sim_pas
+cd ../sim/sim_2_77
+make fresh
+make exes
+chmod +x sim*
+cp sim_c.exe /usr/bin/sim_c
+cp sim_java.exe /usr/bin/sim_java
+cp sim_pasc.exe /usr/bin/sim_pas
+cp sim_text.exe /usr/bin/sim_text
 cd ..
-cp sim1.sh /usr/bin
+cp sim.sh /usr/bin
 chmod +x /usr/bin/sim.sh
+rm /usr/bin/sim_cc /usr/bin/sim_rb /usr/bin/sim_sh
 ln -s /usr/bin/sim_c /usr/bin/sim_cc
-ln -s /usr/bin/sim_c /usr/bin/sim_rb
-ln -s /usr/bin/sim_c /usr/bin/sim_sh
+ln -s /usr/bin/sim_text /usr/bin/sim_rb
+ln -s /usr/bin/sim_text /usr/bin/sim_sh
--- core/sim/sim.sh
+++ core/sim/sim.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 EXTENSION=`echo "$1" | cut -d'.' -f2`
-for i in ../data/$2/ac/*.$EXTENSION
+flag=0;
+max=50;
+for i in `ls -t ../data/$2/ac/*.$EXTENSION ` 
 do
-        sim=`sim_$EXTENSION -p $1 $i |grep ^$1|awk '{print $4}'`
-        if [ ! -z $sim ] && [ $sim -gt 50 ]
+	sim=`sim_$EXTENSION -p $1 $i |grep ^$1|awk '{print $4}'`
+        if [ ! -z $sim ] && [ $sim -eq 100 ]
         then
-                sim_s_id=`basename $i`
-                echo "$sim $sim_s_id" >sim
-                exit $sim
-        fi
-done
-exit 0;
+                 sim_s_id=`basename $i`
+                 echo "$sim $sim_s_id" >sim
+                 exit $sim
 
+        fi
+	if [ ! -z $sim ] && [ $sim -gt $max ]
+	then 
+                 j=$i
+                 max=$sim
+                 flag=1; 
+	fi
+done
+if [ $flag ] && [ $max -gt 50 ]
+then
+                 sim_s_id=`basename $j`
+                 echo "$max $sim_s_id" >sim
+                 exit $max
+fi
+
+
+exit 0;
