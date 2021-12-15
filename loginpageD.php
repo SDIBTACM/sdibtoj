a54fe3540b0275c1f18a58db6565a3c1734b1277
sdibt <sdibtacm@126.com>
2021-12-15 10:09:33 +0800

    refresh vcode

--- loginpageD.php
+++ loginpageD.php
@@ -3,25 +3,30 @@
 <br><br>
 <?
 if (isset($_SESSION['user_id'])){
-	echo "<a href=logout.php>Please logout First!</a>";
-	exit(1);
+        echo "<a href=logout.php>Please logout First!</a>";
+        exit(1);
 }
 ?>
 <form action=loginD.php method=post>
-	<center>
-	<table width=400 algin=center>
-	<tr><td width=25%><?=$MSG_USER_ID?>:<td width=75%><input name="user_id" type="text" size=20></tr>
-	<tr><td><?=$MSG_PASSWORD?>:<td><input name="password" type="password" size=20></tr>
-        <?php if($OJ_VCODE){?>	
+        <center>
+        <table width=400 algin=center>
+        <tr><td width=25%><?=$MSG_USER_ID?>:<td width=75%><input name="user_id" type="text" size=20></tr>
+        <tr><td><?=$MSG_PASSWORD?>:<td><input name="password" type="password" size=20></tr>
+        <?php if($OJ_VCODE){?>
          <tr><td>Code:
                         <td><input name="vcodeD" size=4 type=text>
-                        <img src=vcode.php align="absmiddle">
+                        <img src=vcode.php align="absmiddle" onclick="javascript:newgdcode(this,this.src);">
                 </tr>
           <?php }?>
          <tr><td><td><input name="submit" type="submit" size=10 value="Submit">
         <a href="lostpassword.php">Lost Password</a></tr>
-	</table>
-	<center>
+        </table>
+        <center>
 </form>
 <br><br>
-<?require_once("oj-footer.php");?>
+<script language="javascript">
+    function newgdcode(obj, url) {
+        obj.src = 'vcode.php?nowtime=' + new Date().getTime();
+    }
+</script>
+<?require_once("oj-footer.php");?>                             
